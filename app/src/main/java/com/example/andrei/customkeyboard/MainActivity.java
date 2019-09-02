a427464764f96adc707ededf5815940801af91b8
Asad Mahmood <asad007mahmood@gmail.com>
2019-09-02 01:12:09 -0500

    Fixed some errors

--- app/src/main/java/com/example/andrei/customkeyboard/MainActivity.java
+++ app/src/main/java/com/example/andrei/customkeyboard/MainActivity.java
@@ -8,64 +8,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    DatabaseHelper db;
-    StringBuilder st = new StringBuilder();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                displayit();
-            }
-        });
-    }
-    void displayit()
-    {
-        Cursor res2 = db.getInstance(getApplicationContext()).getAllData_words();
-        while(res2.moveToNext()) {
-            st.append( res2.getString(1).toString() );
-        }
-        showMessage("Text Stored",st.toString());
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void showMessage(String title , String message){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(message);
-        builder.show();
+        final TextView button = (TextView) findViewById(R.id.text_view_id);
 
     }
+
 
 }
